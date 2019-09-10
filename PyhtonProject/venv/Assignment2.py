42f012c67895c082b8ec81bdbc87ebbeaa1ab3e3
surbhipriyadarshini90 <sur.darsh@gmail.com>
2019-09-10 17:54:45 +0530

    10 sep 2nd commit

---
+++ PyhtonProject/venv/Assignment2.py
@@ -0,0 +1,19 @@
+from builtins import print
+
+for a in range(1, 11):
+    print(a)#, end ="")
+
+for b in range(5, 26):
+    print(b)
+
+for c in range(25, 0):
+    print(c)
+
+for d in range(25, -1, -5):
+    print(d)
+
+for e in range(2,101, 2):
+    print(e)
+
+for f in range(1, 100, 2):
+    print(f)
